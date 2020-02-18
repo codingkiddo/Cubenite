b04abee6674c22f3aedb377656ac07ef25ba036d
yasingharsallah <36208939+codingkiddo@users.noreply.github.com>
2020-02-18 17:19:10 +0100

    first commit

---
+++ Assets/Scripts/Score.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour
+{
+    public int scoreNumber = 0 ;
+    public int x = 0;
+    public Text scoreText; 
+    
+    void Update()
+    {
+        x++;
+        if (x % 10 == 0) {
+            scoreNumber++;  
+            scoreText.text = scoreNumber.ToString();
+        }
+       
+    }
+}
